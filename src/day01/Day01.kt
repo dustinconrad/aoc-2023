6318f775dcdc0eae4b0064cd90f7e71ffd8baa60
Dustin Conrad <dustinaconrad@gmail.com>
2023-11-30 23:23:20 -0800

    Regexs are hard

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -9,9 +9,41 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val p = Regex("""\d""")
+    return calibrationValues(p, input)
 }
 
 fun part2(input: List<String>): Int {
-    return input.size
+    val p = Regex("""(?=(\d|one|two|three|four|five|six|seven|eight|nine)).""")
+    return calibrationValues(p, input)
+}
+
+fun calibrationValues(p: Regex, input: List<String>): Int {
+    return input.map { it.matchFirstLast(p) }
+        .map { outer -> outer.toList().map { it.asDigit() } }
+        .map { it.joinToString("") }
+        .sumOf { it.toInt() }
+}
+
+fun String.matchFirstLast(p: Regex): Pair<String, String> {
+    val matches = p.findAll(this).toList()
+    val first = matches.first().groupValues.last()
+    val last = matches.last().groupValues.last()
+    return first to last
+}
+
+fun String.asDigit(): Int {
+    return when {
+        this.length == 1 -> this.toInt()
+        this == "one" -> 1
+        this == "two" -> 2
+        this == "three" -> 3
+        this == "four" -> 4
+        this == "five" -> 5
+        this == "six" -> 6
+        this == "seven" -> 7
+        this == "eight" -> 8
+        this == "nine" -> 9
+        else -> throw IllegalArgumentException("Can't parse $this as digit")
+    }
 }
--- test/day01/Day01Test.kt
+++ test/day01/Day01Test.kt
@@ -8,24 +8,39 @@ class Day01Test {
     @Test
     fun testPart1() {
         val input = """
-            1000
-            2000
-            3000
-
-            4000
-
-            5000
-            6000
-
-            7000
-            8000
-            9000
-
-            10000
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
         """.trimIndent()
             .lines()
 
-        assertEquals(24000, part1(input))
+        assertEquals(142, part1(input))
     }
 
+    @Test
+    fun testPart2() {
+        val input = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent()
+            .lines()
+
+        assertEquals(281, part2(input))
+    }
+
+    @Test
+    fun testPart2EdgeCase() {
+        val input = """
+            trknlxnv43zxlrqjtwonect
+        """.trimIndent()
+            .lines()
+
+        assertEquals(41, part2(input))
+    }
 }
