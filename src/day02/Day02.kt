637e96ddf3142aace59cf1107d6bdf8fe7631ce9
Dustin Conrad <dustinaconrad@gmail.com>
2023-11-30 23:27:34 -0800

    Day 2 prep

---
+++ src/day02/Day02.kt
@@ -0,0 +1,17 @@
+package day02
+
+import readResourceAsBufferedReader
+
+
+fun main() {
+    println("part 1: ${part1(readResourceAsBufferedReader("2_1.txt").readLines())}")
+    println("part 2: ${part2(readResourceAsBufferedReader("2_1.txt").readLines())}")
+}
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
