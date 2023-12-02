b9e990f7ee58bb0dea99e9684a0c551c4bbe0c4c
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-01 21:36:09 -0800

    Day 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -9,9 +9,65 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return 0
+    return validGamesSum(Round(12, 13, 14), input)
 }
 
-fun part2(input: List<String>): Int {
-    return 0
+fun part2(input: List<String>): Long {
+    return input.map { parseGame(it) }
+        .sumOf { it.power }
+}
+
+
+fun validGamesSum(bag: Round, input: List<String>): Int {
+    return input.map { parseGame(it) }
+        .filter { it.validFor(bag) }
+        .sumOf { it.id }
+}
+
+
+data class Round(
+    val red: Int,
+    val green: Int,
+    val blue: Int
+)
+
+data class CubeGame(
+    val id: Int,
+    val rounds: List<Round>
+) {
+    val maxRed = rounds.maxBy { it.red }.red
+    val maxGreen = rounds.maxBy { it.green }.green
+    val maxBlue = rounds.maxBy { it.blue }.blue
+
+    val power: Long = maxRed.toLong() * maxGreen.toLong() * maxBlue.toLong()
+
+    fun validFor(bag: Round): Boolean =
+        maxRed <= bag.red && maxGreen <= bag.green && maxBlue <= bag.blue
+}
+
+fun parseGame(line: String): CubeGame {
+    val (gameId, rest) = line.split(":")
+    val rounds = rest.split(";")
+    return CubeGame(gameId.split(" ")[1].toInt(), rounds.map { parseRound(it) })
+}
+
+val red = Regex("""\d+ red""")
+val green = Regex("""\d+ green""")
+val blue = Regex("""\d+ blue""")
+
+fun parseRound(round: String): Round {
+    val cubes = round.split(",").map { it.trim() }
+    val redCount = cubes.filter { red.matches(it) }
+        .map { it.split(" ")[0].toInt() }
+        .firstOrNull() ?: 0
+
+    val greenCount = cubes.filter {green.matches(it) }
+        .map { it.split(" ")[0].toInt() }
+        .firstOrNull() ?: 0
+
+    val blueCount = cubes.filter { blue.matches(it) }
+        .map { it.split(" ")[0].toInt() }
+        .firstOrNull() ?: 0
+
+    return Round(redCount, greenCount, blueCount)
 }
--- test/day02/Day02Test.kt
+++ test/day02/Day02Test.kt
@@ -1,4 +1,4 @@
-package day01
+package day02
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -8,11 +8,15 @@ class Day02Test {
     @Test
     fun testPart1() {
         val input = """
-            
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         """.trimIndent()
             .lines()
 
-        assertEquals(142, part1(input))
+        assertEquals(8, part1(input))
     }
 
     @Test
