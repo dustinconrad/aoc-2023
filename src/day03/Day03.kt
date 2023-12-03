356352b56c5af96546bf48bc66654b3eb2656565
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-02 22:45:16 -0800

    Day 3

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -1,6 +1,8 @@
 package day03
 
 import readResourceAsBufferedReader
+import kotlin.math.max
+import kotlin.math.min
 
 
 fun main() {
@@ -8,10 +10,91 @@ fun main() {
     println("part 2: ${part2(readResourceAsBufferedReader("3_1.txt").readLines())}")
 }
 
+typealias Coord2 = Pair<Int,Int>
+
+typealias PartCoord = Pair<Int, IntRange>
+
+typealias PartNumber = Pair<PartCoord, Int>
+
+fun partNumber(y: Int, r: IntRange, v: Int) = (y to r) to v
+
+val numberRegex = Regex("""\d+""")
+val symbol = Regex("""[^\d.]""")
+
+data class EngineSchematic(
+    val grid: List<String>
+) {
+
+    val partNumbers: List<PartNumber> = grid.flatMapIndexed { idx, line ->
+        numberRegex.findAll(line).map { mr ->
+            partNumber(idx, mr.range, mr.value.toInt())
+        }.toList()
+    }
+
+    val validPartNumbers = partNumbers.filter { isValidPn(it) }
+
+    val validPartsByRow = validPartNumbers.groupBy { it.first.first }
+
+    fun isValidPc(pc: PartCoord): Boolean {
+        val minX = max(0, pc.second.first - 1)
+        val maxX = min(grid[0].length - 1, pc.second.last + 1)
+
+        val xRange = IntRange(minX, maxX)
+
+        if (pc.first > 0 && symbol.containsMatchIn(grid[pc.first - 1].substring(xRange))) {
+            return true
+        }
+        if (symbol.matches(grid[pc.first][minX].toString())) {
+            return true
+        }
+        if (symbol.matches(grid[pc.first][maxX].toString())) {
+            return true
+        }
+        if (pc.first < grid.size - 1 && symbol.containsMatchIn(grid[pc.first + 1].substring(xRange))) {
+            return true
+        }
+        return false
+    }
+
+    fun isValidPn(pn: PartNumber): Boolean = isValidPc(pn.first)
+
+    val gears: List<Coord2> = grid.flatMapIndexed { idx, line ->
+        line.mapIndexed { idx, c -> idx to c }
+            .filter { it.second == '*' }
+            .map { idx to it.first }
+    }
+
+    fun partsForGear(coord: Coord2): List<PartNumber> {
+        val minX = max(0, coord.second - 1)
+        val maxX = min(grid[0].length - 1, coord.second + 1)
+        val range = IntRange(minX, maxX)
+
+        val gears = mutableListOf<PartNumber>()
+
+        fun partsForRow(r: Int): List<PartNumber> = validPartsByRow.getOrDefault(r, emptyList())
+            .filter { range.overlaps(it.first.second) }
+
+        gears.addAll(partsForRow(coord.first - 1))
+        gears.addAll(partsForRow(coord.first))
+        gears.addAll(partsForRow(coord.first + 1))
+
+        return gears.toList()
+    }
+
+    fun validGears(): List<Pair<PartNumber, PartNumber>> = gears.map { partsForGear(it) }
+            .filter { it.size == 2 }
+            .map { it[0] to it[1] }
+
+}
+
+fun IntRange.overlaps(other: IntRange): Boolean = !(this.first > other.last || this.last < other.first)
+
 fun part1(input: List<String>): Int {
-    return 0
+    val engine = EngineSchematic(input)
+    return engine.validPartNumbers.sumOf { it.second }
 }
 
 fun part2(input: List<String>): Int {
-    return 0
+    val engine = EngineSchematic(input)
+    return engine.validGears().sumOf { it.first.second * it.second.second }
 }
--- test/day03/Day03Test.kt
+++ test/day03/Day03Test.kt
@@ -8,10 +8,39 @@ class Day03Test {
     @Test
     fun testPart1() {
         val input = """
+            467..114..
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..
         """.trimIndent()
             .lines()
 
-        assertEquals(8, part1(input))
+        assertEquals(4361, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+            467..114..
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..
+        """.trimIndent()
+            .lines()
+
+        assertEquals(467835, part2(input))
     }
     
 }
