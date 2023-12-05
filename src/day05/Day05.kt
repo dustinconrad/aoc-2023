83266ec61a619ccd1e26ea2f1642db30293a8805
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-04 21:48:16 -0800

    Day 5 part 1

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,5 +1,6 @@
 package day05
 
+import byEmptyLines
 import readResourceAsBufferedReader
 import kotlin.math.max
 import kotlin.math.min
@@ -7,13 +8,80 @@ import kotlin.math.min
 
 fun main() {
     println("part 1: ${part1(readResourceAsBufferedReader("5_1.txt").readLines())}")
-    println("part 2: ${part2(readResourceAsBufferedReader("5_1.txt").readLines())}")
+    //println("part 2: ${part2(readResourceAsBufferedReader("5_1.txt").readLines())}")
 }
 
-fun part1(input: List<String>): Int {
-    return 0
+fun part1(input: List<String>): Long {
+    val byEmpty = input.byEmptyLines();
+    val seeds = byEmpty.first().split(" ").drop(1).map { it.toLong() }
+    val almanac = Almanac.parse(byEmpty.drop(1))
+    return seeds.minOf {
+        almanac.lookup(it)
+    }
 }
 
-fun part2(input: List<String>): Int {
-    return 0
+fun part2(input: List<String>): Long {
+    val byEmpty = input.byEmptyLines();
+    val seeds = byEmpty.first().split(" ").drop(1).map { it.toLong() }
+
+
+    val almanac = Almanac.parse(byEmpty.drop(1))
+    return seeds.minOf {
+        almanac.lookup(it)
+    }
+}
+
+data class Almanac(val mappers: List<Mapper>) {
+
+    fun lookup(seed: Long): Long = mappers.fold(seed) { acc, mapper ->
+        println("$acc -> ${mapper.name} -> ${mapper.lookup(acc)}")
+        mapper.lookup(acc)
+    }
+
+    companion object {
+        fun parse(mappers: List<String>): Almanac {
+            return Almanac(mappers.map { Mapper.parse(it) })
+        }
+    }
+
+}
+
+data class Mapper(
+    val name: String,
+    val rules: List<MappingRule>
+) {
+
+    fun lookup(v: Long): Long {
+        return rules.firstNotNullOfOrNull { it.map(v) } ?: v
+    }
+
+    companion object {
+        fun parse(map: String): Mapper {
+            val lines = map.lines()
+            val id = lines.first().split(" ").first()
+            val rules = lines.drop(1)
+                .map { MappingRule.parse(it) }
+            return Mapper(id, rules)
+        }
+    }
+}
+
+data class MappingRule(val dst: Long, val src: Long, val l: Long) {
+
+    private val srcRange = LongRange(src, src + l - 1)
+
+    fun map(i: Long): Long? {
+        return if (srcRange.contains(i)) {
+            dst + (i - srcRange.first)
+        } else {
+            null
+        }
+    }
+
+    companion object {
+        fun parse(line: String): MappingRule {
+            val (d, s, l) = line.split(" ").map { it.toLong() }
+            return MappingRule(d, s, l)
+        }
+    }
 }
--- test/day05/Day05Test.kt
+++ test/day05/Day05Test.kt
@@ -8,10 +8,43 @@ class Day05Test {
     @Test
     fun testPart1() {
         val input = """
+            seeds: 79 14 55 13
+
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+
+            soil-to-fertilizer map:
+            0 15 37
+            37 52 2
+            39 0 15
+
+            fertilizer-to-water map:
+            49 53 8
+            0 11 42
+            42 0 7
+            57 7 4
+
+            water-to-light map:
+            88 18 7
+            18 25 70
+
+            light-to-temperature map:
+            45 77 23
+            81 45 19
+            68 64 13
+
+            temperature-to-humidity map:
+            0 69 1
+            1 0 69
+
+            humidity-to-location map:
+            60 56 37
+            56 93 4
         """.trimIndent()
             .lines()
 
-        assertEquals(13, part1(input))
+        assertEquals(35, part1(input))
     }
 
     @Test
@@ -20,7 +53,7 @@ class Day05Test {
         """.trimIndent()
             .lines()
 
-        assertEquals(30, part2(input))
+        assertEquals(30, part1(input))
     }
     
 }
