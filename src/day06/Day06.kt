fca7bee0b9f1bfb5dce6e5c66fe9710791eb6784
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-05 21:35:54 -0800

    6.1 done

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -8,10 +8,46 @@ fun main() {
     println("part 2: ${part2(readResourceAsBufferedReader("6_1.txt").readLines())}")
 }
 
+val whitespace = Regex("\\s+")
+
+data class Race(val time: Long, val distance: Long) {
+
+    fun race(wait: Long): Long {
+        val speed = wait
+        val remaining = time - wait
+        return remaining * speed
+        //return wait*time - wait*wait
+    }
+
+    fun winners(): List<Long> = (1 until distance).map { race(it) }
+        .filter { it > distance }
+
+}
+
+fun parseRaces(input: List<String>): List<Race> {
+    val (timeString, distanceString) = input
+    val times = whitespace.split(timeString.split(":")[1].trim()).map { it.toLong() }
+    val distances = whitespace.split(distanceString.split(":")[1].trim()).map { it.toLong() }
+
+    return times.zip(distances).map { Race(it.first, it.second) }
+}
+
+fun parseRace(input: List<String>): Race {
+    val (timeString, distanceString) = input
+    val time = whitespace.replace(timeString.split(":")[1], "").toLong()
+    val distance = whitespace.replace(distanceString.split(":")[1], "").toLong()
+
+    return Race(time, distance)
+}
+
 fun part1(input: List<String>): Int {
-    return 0
+    val races = parseRaces(input)
+    return races.map { it.winners().size }
+        .reduce(Int::times)
 }
 
 fun part2(input: List<String>): Int {
-    return 0
+    val races = parseRace(input)
+    return races.winners().size
 }
+
