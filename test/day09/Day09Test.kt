9792977a00dc61642a77a8466333a0781ee900ca
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-07 23:14:47 -0800

    Day 9 prep

---
+++ test/day09/Day09Test.kt
@@ -0,0 +1,26 @@
+package day09
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day09Test {
+
+    @Test
+    fun testPart1() {
+        val input = """
+        """.trimIndent()
+            .lines()
+
+        assertEquals(142, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+        """.trimIndent()
+            .lines()
+
+        assertEquals(281, part2(input))
+    }
+    
+}
