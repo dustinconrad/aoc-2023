916e8ec7b62398f46b85e4ed77adca5669a35170
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-07 22:14:19 -0800

    Math

--- src/Utils.kt
+++ src/Utils.kt
@@ -94,3 +94,45 @@ fun quadratic(a: Long, b: Long, c: Long): List<Double> {
 
     return listOf(minus, plus).sorted()
 }
+data class ExtendedEuclidianResult(
+    val gcd: Long,
+    val s: Long,
+    val t: Long
+)
+
+fun extendedEuclidean(a: Long, b: Long): ExtendedEuclidianResult {
+    var ri = b
+    var riSubOne = a
+    var si = 0L
+    var siSubOne = 1L
+    var ti = 1L
+    var tiSubOne = 0L
+
+    while (ri != 0L) {
+        val quotient = riSubOne / ri
+        val riPlusOne = riSubOne - quotient * ri
+        val siPlusOne = siSubOne - quotient * si
+        val tiPlusOne = tiSubOne - quotient * ti
+
+        riSubOne = ri
+        siSubOne = si
+        tiSubOne = ti
+
+        ri = riPlusOne;
+        si = siPlusOne;
+        ti = tiPlusOne;
+    }
+
+    return ExtendedEuclidianResult(
+        riSubOne,
+        siSubOne,
+        tiSubOne
+    )
+}
+
+fun lcm(vararg numbers: Long): Long {
+    val (a, b) = numbers
+    val gcd = extendedEuclidean(a, b).gcd
+    val result = (a * b) / gcd;
+    return numbers.drop(2).fold(result) { acc, n -> lcm(acc, n) }
+}
