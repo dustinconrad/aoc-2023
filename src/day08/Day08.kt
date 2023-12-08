3577205ca3ed9594f4a5b9297dd328dbf065e51c
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-07 21:31:38 -0800

    Day 8 part 1

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -1,5 +1,6 @@
 package day08
 
+import byEmptyLines
 import readResourceAsBufferedReader
 
 
@@ -8,8 +9,53 @@ fun main() {
     println("part 2: ${part2(readResourceAsBufferedReader("8_1.txt").readLines())}")
 }
 
+data class DesertMap(val nodes: Map<String, Pair<String, String>>) {
+
+    fun traverse(instr: String): List<String> {
+        val endlessInstructions = sequence {
+            while (true) {
+                yieldAll(instr.asSequence())
+            }
+        }
+
+        val visits = endlessInstructions.scan("AAA") { acc, c ->
+            val next = nodes[acc]!!
+            when(c) {
+                'R' -> next.second
+                'L' -> next.first
+                else -> throw IllegalArgumentException("Unknown direction $c")
+            }
+        }
+
+        val steps = visits.takeWhile { it != "ZZZ" }.toList()
+
+        return steps
+    }
+
+    companion object {
+
+        val node = Regex("""(\w+) = \((\w+), (\w+)\)""")
+        fun parse(input: String): DesertMap {
+            fun parseNode(line: String): Triple<String, String, String> {
+                val result = node.matchEntire(line)
+                val (_, n, l, r) = result!!.groupValues
+                return Triple(n, l, r)
+            }
+
+            val nodes = input.split("\n")
+                .map { parseNode(it) }
+                .map { it.first to Pair(it.second, it.third) }
+                .toMap()
+
+            return DesertMap(nodes)
+        }
+    }
+}
+
 fun part1(input: List<String>): Int {
-    return 0
+    val (directions, map) = input.byEmptyLines()
+    val desertMap = DesertMap.parse(map)
+    return desertMap.traverse(directions).size
 }
 
 fun part2(input: List<String>): Int {
