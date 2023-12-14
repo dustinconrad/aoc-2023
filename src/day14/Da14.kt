a9057416ba849c7e1e8f9864b4270e3decfc6c53
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-13 21:37:09 -0800

    14.1 done

--- src/day14/Da14.kt
+++ src/day14/Da14.kt
@@ -8,8 +8,40 @@ fun main() {
     println("part 2: ${part2(readResourceAsBufferedReader("14_1.txt").readLines())}")
 }
 
+data class Dish(val grid: List<String>) {
+
+    val columns = grid[0].indices.map { idx -> grid.map { it[idx] }.joinToString("") }
+
+    fun tiltNorth(): Int {
+        val result = Array<MutableMap<Int, Int>>(columns.size) { mutableMapOf() }
+        for (col in columns.withIndex()) {
+            var lastRectangle = -1
+            var roundCount = 0
+            for (y in columns.indices) {
+                when (col.value[y]) {
+                    '#' -> {
+                        result[col.index][lastRectangle] = roundCount
+                        lastRectangle = y
+                        roundCount = 0
+                    }
+                    'O' -> roundCount++
+                }
+            }
+            result[col.index][lastRectangle] = roundCount
+        }
+        return result.map {
+            it.entries.map { (anchor, count) ->
+                val max = columns[0].length - (anchor + 1)
+                (0 until count).sumOf { max - it }
+            }.sum()
+        }.sum()
+    }
+
+}
+
 fun part1(input: List<String>): Int {
-    return 0
+    val dish = Dish(input)
+    return dish.tiltNorth()
 }
 
 fun part2(input: List<String>): Int {
