41661efcfcc646a533cb8d9660bb050a71a9f4df
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-13 22:01:18 -0800

    refactored

--- src/day14/Da14.kt
+++ src/day14/Da14.kt
@@ -12,36 +12,42 @@ data class Dish(val grid: List<String>) {
 
     val columns = grid[0].indices.map { idx -> grid.map { it[idx] }.joinToString("") }
 
-    fun tiltNorth(): Int {
-        val result = Array<MutableMap<Int, Int>>(columns.size) { mutableMapOf() }
+    fun tiltPart1(): Map<Pair<Int, Int>, List<Pair<Int, Int>>> {
+        val result = mutableMapOf<Pair<Int, Int>, MutableList<Pair<Int, Int>>>()
         for (col in columns.withIndex()) {
             var lastRectangle = -1
-            var roundCount = 0
             for (y in columns.indices) {
                 when (col.value[y]) {
-                    '#' -> {
-                        result[col.index][lastRectangle] = roundCount
-                        lastRectangle = y
-                        roundCount = 0
+                    '#' -> lastRectangle = y
+                    'O' -> {
+                        result.compute(lastRectangle to col.index) { k, v ->
+                            if (v == null) {
+                                mutableListOf(lastRectangle + 1 to col.index)
+                            } else {
+                                v.add(lastRectangle + v.size to col.index)
+                                v
+                            }
+                        }
                     }
-                    'O' -> roundCount++
                 }
             }
-            result[col.index][lastRectangle] = roundCount
         }
-        return result.map {
-            it.entries.map { (anchor, count) ->
-                val max = columns[0].length - (anchor + 1)
-                (0 until count).sumOf { max - it }
-            }.sum()
-        }.sum()
+        return result
     }
 
+    fun part1(): Int {
+        val part1Result = tiltPart1()
+        return part1Result.map { (anchor, stones) ->
+            val count = stones.count()
+            val max = grid.size - (anchor.first + 1)
+            (0 until count).sumOf { max - it }
+        }.sum()
+    }
 }
 
 fun part1(input: List<String>): Int {
     val dish = Dish(input)
-    return dish.tiltNorth()
+    return dish.part1()
 }
 
 fun part2(input: List<String>): Int {
