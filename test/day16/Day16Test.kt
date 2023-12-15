d5920d16dc036024ca3c1f4768b23087b1a549a2
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-14 23:20:40 -0800

    prep

---
+++ test/day16/Day16Test.kt
@@ -0,0 +1,25 @@
+package day16
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day16Test {
+
+    @Test
+    fun testPart1() {
+        val input = """
+        """.trimIndent()
+            .lines()
+
+        assertEquals(142, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+        """.trimIndent()
+            .lines()
+
+        assertEquals(281, part2(input))
+    }
+}
