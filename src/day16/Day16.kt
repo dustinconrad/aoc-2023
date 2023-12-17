2f4b1d676967c3f3585d24cb8c70b4d897b9ea70
Dustin Conrad <dustinaconrad@gmail.com>
2023-12-16 20:56:58 -0800

    Day 16 stub

--- src/day16/Day16.kt
+++ src/day16/Day16.kt
@@ -15,3 +15,35 @@ fun part1(input: List<String>): Int {
 fun part2(input: List<String>): Int {
     return 0
 }
+
+data class LightPath(val grid: List<String>) {
+
+    fun path(): List<Pair<Int,Int>> {
+        var curr = 0 to 0
+        var currVec = 0 to 1
+
+        val result = mutableListOf<Pair<Int,Int>>()
+
+        while (true) {
+            result.add(curr)
+
+        }
+
+    }
+
+    fun nextVec(curr: Pair<Int, Int>, currVec: Pair<Int,Int>): List<Pair<Int,Int>> {
+        val tile = grid[curr.first][curr.second]
+        val (cy, cx) = currVec
+        return when {
+            tile == '/' -> listOf(-cx to -cy)
+            tile == '\\' -> listOf(cy to cx)
+            cy != 0 && tile == '-' ->
+        }
+    }
+
+}
+
+fun path(grid: List<String>): List<Pair<Int,Int>> {
+
+}
+
