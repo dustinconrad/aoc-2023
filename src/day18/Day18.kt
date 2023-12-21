814c1fd9934ecfbde338acdef35da22f4b7f3911
Dustin Conrad <dustin_conrad@discovery.com>
2023-12-21 14:11:26 -0800

    Day 18 prep

---
+++ src/day18/Day18.kt
@@ -0,0 +1,17 @@
+package day18
+
+import readResourceAsBufferedReader
+
+
+fun main() {
+    println("part 1: ${part1(readResourceAsBufferedReader("18_1.txt").readLines())}")
+    println("part 2: ${part2(readResourceAsBufferedReader("18_1.txt").readLines())}")
+}
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
---
+++ test/day18/Day18Test.kt
@@ -0,0 +1,26 @@
+package day18
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day18Test {
+
+    @Test
+    fun testPart1() {
+        val input = """
+        """.trimIndent()
+            .lines()
+
+        assertEquals(142, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+        """.trimIndent()
+            .lines()
+
+        assertEquals(281, part2(input))
+    }
+
+}
